537ed9b1a52b5ede361196abbf6cd1f709bee17d
Talha Ismail <talha.ismail2119@gmail.com>
2024-01-07 20:06:06 +0600

    Hive_Basics

---
+++ widgets/contact_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ContactList extends StatefulWidget {
+  const ContactList({super.key});
+
+  @override
+  State<ContactList> createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
